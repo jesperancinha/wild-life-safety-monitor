e64d6e90f122c58f725f21cc893951f758739fc2
jesperancinha <jofisaes@gmail.com>
2024-03-22 09:28:20 +0100

    Creates different ports

--- wlsm-aggregator-service/src/main/java/org/jesperancinha/wlsmaggregatorservice/WildLifeSafetyMonitorAggregatorServiceApplication.java
+++ wlsm-aggregator-service/src/main/java/org/jesperancinha/wlsmaggregatorservice/WildLifeSafetyMonitorAggregatorServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WildLifeSafetyMontorAggregatorServiceApplication {
+public class WildLifeSafetyMonitorAggregatorServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WildLifeSafetyMontorAggregatorServiceApplication.class, args);
+		SpringApplication.run(WildLifeSafetyMonitorAggregatorServiceApplication.class, args);
 	}
 
 }
