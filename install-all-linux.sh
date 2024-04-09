4e817c7dd5670978665a9850f98af43a030743ca
jesperancinha <jofisaes@gmail.com>
2024-04-09 17:54:18 +0200

    Comments potentially unnecessary

--- install-all-linux.sh
+++ install-all-linux.sh
@@ -11,7 +11,7 @@ curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl helm
-sudo apt-mark hold kubelet kubeadm kubectl helm
+#sudo apt-mark hold kubelet kubeadm kubectl helm
 cd ~ || exit;
 curl -L https://kuma.io/installer.sh | VERSION=2.6.1 sh -
 echo "---------------------------------------------------------------------------"
