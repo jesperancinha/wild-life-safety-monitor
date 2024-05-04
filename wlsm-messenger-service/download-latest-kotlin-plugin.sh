9aacd45f7c5c4719478b306e62b816996b5f020d
jesperancinha <jofisaes@gmail.com>
2024-05-04 18:41:33 +0200

    Some tweaks

--- wlsm-messenger-service/download-latest-kotlin-plugin.sh
+++ wlsm-messenger-service/download-latest-kotlin-plugin.sh
@@ -4,5 +4,6 @@ curl "https://search.maven.org/solrsearch/select?q=g:io.grpc+AND+a:protoc-gen-gr
 # shellcheck disable=SC2002
 version=$(cat versionInfo.json | jq -r '.response.docs[0].latestVersion')
 curl -O "https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-kotlin/""$version""/protoc-gen-grpc-kotlin-""$version""-jdk8.jar"
+# shellcheck disable=SC2002
 cat plugin-template.sh | sed s/SOME_VERSION/"${version}"/g > protoc-gen-grpc-kotlin.sh
 chmod +x protoc-gen-grpc-kotlin.sh
