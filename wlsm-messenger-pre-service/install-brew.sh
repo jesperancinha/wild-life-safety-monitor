b6b4c4b29610462b49478424474d4759f6cecec1
jesperancinha <jofisaes@gmail.com>
2024-05-06 10:33:00 +0200

    Brew update

---
+++ wlsm-messenger-pre-service/install-brew.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
---
+++ wlsm-messenger-pre-service/setup-brew-linux.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
