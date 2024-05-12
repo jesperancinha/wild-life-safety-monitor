3909afa7ea32941380e17ac542e9813e68dddfe4
jesperancinha <jofisaes@gmail.com>
2024-05-12 10:25:22 +0200

    Adds envoy script as an example

--- wlsm-messenger-frontend/client.js
+++ wlsm-messenger-frontend/client.js
@@ -1,7 +1,7 @@
 const {Message} = require('./messenger_pb.js');
 const {MessengerClient} = require('./messenger_grpc_web_pb.js');
 
-const client = new MessengerClient("http://localhost:50051", null, null);
+const client = new MessengerClient("http://localhost:8080", null, null);
 
 const request = new Message();
 request.setText('Hello there!');
