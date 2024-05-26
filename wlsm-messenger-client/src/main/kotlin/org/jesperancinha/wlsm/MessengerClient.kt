ff68ebe4a206111e40a938b9ea662f9e52133afb
jesperancinha <jofisaes@gmail.com>
2024-05-26 18:23:58 +0200

    Protobuf installation

--- wlsm-messenger-client/src/main/kotlin/org/jesperancinha/wlsm/MessengerClient.kt
+++ wlsm-messenger-client/src/main/kotlin/org/jesperancinha/wlsm/MessengerClient.kt
@@ -25,8 +25,8 @@ class MessengerClient(private val channel: ManagedChannel) : Closeable {
 }
 
 suspend fun main(args: Array<String>) {
-    val port = System.getenv("PORT")?.toInt() ?: 50051
-//    val port = System.getenv("PORT")?.toInt() ?: 8000
+//    val port = System.getenv("PORT")?.toInt() ?: 50051
+    val port = System.getenv("PORT")?.toInt() ?: 8000
 
     val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build()
 
