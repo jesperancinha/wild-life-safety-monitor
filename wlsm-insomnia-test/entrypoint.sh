8141e34c40159a4194af20a2b046e791d9ed184f
jesperancinha <jofisaes@gmail.com>
2024-07-03 17:04:04 +0200

    Oeps..

--- wlsm-insomnia-test/entrypoint.sh
+++ wlsm-insomnia-test/entrypoint.sh
@@ -5,14 +5,3 @@ do
   echo | inso run test --src InsomniaListener.json --verbose --env "OpenAPI env wlsm-listener-deployment.wlsm-namespace.svc.cluster.local:8080"
 done
 echo | inso run test --src Insomnia.json --verbose --env "OpenAPI env wlsm-aggregator-deployment.wlsm-namespace.svc.cluster.local:8082"
-
-export LC_CTYPE="en_US.UTF-8"
-
-result=$(kubectl get pods --sort-by=.status.startTime -n wlsm-namespace -o jsonpath='{.items[-1].status.containerStatuses[0].state.terminated.reason}')
-
-podname=$(kubectl get pods --sort-by=.status.startTime -n wlsm-namespace -o jsonpath='{.items[-1].metadata.name}')
-podname=$(echo $podname)
-kubectl logs $podname -n wlsm-namespace;
-if [ "$result" != 'Completed' ]; then
-  exit 1
-fi
