39b2f87d38d57023b736a5c00d31ebfc9c94a76b
jesperancinha <jofisaes@gmail.com>
2024-07-03 16:56:04 +0200

    Wait for terminal status

--- wlsm-insomnia-test/showResults.sh
+++ wlsm-insomnia-test/showResults.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env sh
 
-result=$(kubectl get pods --sort-by=.status.startTime -n wlsm-namespace -o jsonpath='{.items[-1].status.containerStatuses[0].state.terminated.reason}')
 podname=$(kubectl get pods --sort-by=.status.startTime -n wlsm-namespace -o jsonpath='{.items[-1].metadata.name}')
 podname=$(echo $podname)
-kubectl logs $podname -n wlsm-namespace;
+namespace="wlsm-namespace"
+
+get_pod_status() {
+    kubectl get pod "$podname" -n "$namespace" -o jsonpath='{.status.phase}'
+}
+
+while true; do
+    status=$(get_pod_status)
+    echo "Current status of pod $podname: $status"
+    if [ "$status" = 'Succeeded' ] || [ "$status" = 'Failed' ] || [ "$status" = 'Error' ]; then
+        echo "Pod $podname has reached a terminal status: $status"
+        break
+    fi
+    sleep 5
+done
+
+result=$(kubectl get pods --sort-by=.status.startTime -n wlsm-namespace -o jsonpath='{.items[-1].status.containerStatuses[0].state.terminated.reason}')
+kubectl logs $podname -n $namespace;
 if [ "$result" != 'Completed' ]; then
   exit 1
 fi
