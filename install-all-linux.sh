cafb3d3e614fc72d6f50215458caaaa2663a339b
jesperancinha <jofisaes@gmail.com>
2024-07-03 07:48:38 +0200

    Gets latest versions of all

--- install-all-linux.sh
+++ install-all-linux.sh
@@ -9,12 +9,12 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 k8sVersion=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 k8sVersion=$(echo "$k8sVersion" | awk -F '.' '{print $1 "." $2}')
 echo "Installing Kubernetes version $k8sVersion"
-
 curl -fsSL https://pkgs.k8s.io/core:/stable:/"$k8sVersion"/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/'$k8sVersion'/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl helm
 #sudo apt-mark hold kubelet kubeadm kubectl helm
