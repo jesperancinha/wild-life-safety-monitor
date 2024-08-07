471a770e1b7456c75eb586217cb7d65d19ad73c8
jesperancinha <jofisaes@gmail.com>
2024-08-07 11:55:09 +0200

    It is now --exportFile

--- wlsm-insomnia-test/entrypoint.sh
+++ wlsm-insomnia-test/entrypoint.sh
@@ -2,6 +2,6 @@
 max=10
 for i in $(seq 1 $max)
 do
-  echo | inso run test --src InsomniaListener.json --verbose --env "OpenAPI env wlsm-listener-deployment.wlsm-namespace.svc.cluster.local:8080"
+  echo | inso run test --exportFile InsomniaListener.json --verbose --env "OpenAPI env wlsm-listener-deployment.wlsm-namespace.svc.cluster.local:8080"
 done
-echo | inso run test --src Insomnia.json --verbose --env "OpenAPI env wlsm-aggregator-deployment.wlsm-namespace.svc.cluster.local:8082"
+echo | inso run test --exportFile Insomnia.json --verbose --env "OpenAPI env wlsm-aggregator-deployment.wlsm-namespace.svc.cluster.local:8082"
